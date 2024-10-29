953df14f1c4606027e41d7333c7c1056bb02a2cd
Eduardo20210681LALO <20210681@uthh.edu.mx>
2024-10-29 00:35:33 -0600

    Version 1.3.2 Subiendo Login Funcional

--- app/Inicio/Login.tsx
+++ app/Inicio/Login.tsx
@@ -5,72 +5,109 @@ import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { TextInput, Card, Text } from 'react-native-paper';
 import { ThemedText } from '@/components/ThemedText';
 import { useRouter } from 'expo-router';
+import { ToastAndroid } from 'react-native';
 
 import Toast from 'react-native-toast-message';
-
 
 export default function Login() {
     const router = useRouter();
 
+    const [usuario, setUsuario] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
-    const [mostrarContrasenia, setMostrarContrasenia] = useState(false);
 
-    const [bloqueado, setBloqueado] = useState(false);
-    const [tiempoRestante, setTiempoRestante] = useState(0);
+    const [mostrarContrasenia, setMostrarContrasenia] = useState(false); // para la funcion de mostrar contraseña
 
     const toggleMostrarContrasenia = () => {
-        setMostrarContrasenia(!mostrarContrasenia);
+        setMostrarContrasenia(!mostrarContrasenia); // funcion de mostrar contraseña
     };
 
-    const handleLogin = () => {
-        console.log('Se redirigira a home')
-        router.push('/Inicio/DrawerNavigator');
-    };
+    const IniciarSesión = async () => {
+        try {
+            const response = await fetch('http://192.168.1.146/TeleSecundaria763Movil/Inicio/login.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    usuario: usuario,
+                    email: email,
+                    password: password,
+                }),
+            });
+    
+            if (response.ok) {
+                const data = await response.json();
+                if (data.success) {
+                    
+                    console.log("Inicio de sesión exitoso");
 
-    // aqui ira el procedimiento del login
+
+
+
+                } else {
+                    console.log("Error de inicio de sesión:", data.messages);
+                }
+            } else {
+                console.log('Error en la solicitud, intente nuevamente.');
+                console.error('Error en la solicitud:', response.status);
+            }
+        } catch (error) {
+            console.error('Error en el catch:', error);
+            console.log('Error en el servidor, intente más tarde.');
+        }
+    };
+    
 
     const Validacion = async () => {
-        const gmailRegex = /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
-        if (!email) {
+        const usuarioRegex = /^[a-zA-Z0-9]+$/;
+        const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[#\$%&])[A-Za-z\d#\$%&]+$/;
+
+        if (!usuario) {
             Toast.show({
-                type: 'info',
-                text1: 'Advertencia',
-                text2: 'Por favor, ingrese un correo electrónico.',
-                position: 'bottom',
-                bottomOffset: 350,
-                visibilityTime: 3000,
+                type: 'info', text1: 'Advertencia', text2: 'Por favor, ingrese un nombre de usuario.',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
             });
             return;
         }
-        if (!gmailRegex.test(email)) {
+        if (!email) {
             Toast.show({
-                type: 'info',
-                text1: 'Advertencia',
-                text2: 'Por favor, ingrese un correo electrónico de Gmail válido.',
-                position: 'bottom',
-                bottomOffset: 350,
-                visibilityTime: 3000,
+                type: 'info', text1: 'Advertencia', text2: 'Por favor, ingrese un correo electrónico.',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
             });
             return;
         }
         if (!password) {
             Toast.show({
-                type: 'info',
-                text1: 'Advertencia',
-                text2: 'Por favor, ingrese una contraseña.',
-                position: 'bottom',
-                bottomOffset: 350,
-                visibilityTime: 3000,
+                type: 'info', text1: 'Advertencia', text2: 'Por favor, ingrese una contraseña.',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
             });
             return;
         }
-
-        await handleLogin();
+        if (!usuarioRegex.test(usuario)) {
+            Toast.show({
+                type: 'info', text1: 'Advertencia', text2: 'El nombre de usuario no valido',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
+            });
+            return;
+        }
+        if (!emailRegex.test(email)) {
+            Toast.show({
+                type: 'info', text1: 'Advertencia', text2: 'Por favor, ingrese un correo electrónico válido.',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
+            });
+            return;
+        }
+        if (!passwordRegex.test(password)) {
+            Toast.show({
+                type: 'info', text1: 'Advertencia', text2: 'La contraseña no valida',
+                position: 'bottom', bottomOffset: 350, visibilityTime: 3000,
+            });
+            return;
+        }
+        await IniciarSesión();
     };
-    
-
- 
 
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1, backgroundColor: '#f0f0f0' }}>
@@ -84,19 +121,29 @@ export default function Login() {
                         <ThemedText type="defaultSemiBold" style={styles.subtitle}>
                             Por favor, inicie sesión para continuar
                         </ThemedText>
-
                   
+                        {/* Usuario */}
+                        <View style={styles.inputContainer}>
+                            <TextInput
+                                label="Usuario"
+                                placeholder="Ingrese su nombre de usuario"
+                                mode="outlined"
+                                value={usuario}
+                                onChangeText={setUsuario}
+                                keyboardType="default"
+                            />
+                        </View>
+
                         {/* Correo electrónico */}
                         <View style={styles.inputContainer}>
-                        <TextInput
-                            label="Correo Electrónico"
-                            placeholder="Ingrese su correo electrónico"
-                            mode="outlined"
-                            value={email}
-                            onChangeText={setEmail}
-                            keyboardType="email-address" // Configura el teclado para correo electrónico
-                        />
-
+                            <TextInput
+                                label="Correo Electrónico"
+                                placeholder="Ingrese su correo electrónico"
+                                mode="outlined"
+                                value={email}
+                                onChangeText={setEmail}
+                                keyboardType="email-address"
+                            />
                         </View>
 
                         {/* Contraseña */}
@@ -124,8 +171,8 @@ export default function Login() {
                         </View>
 
                         <TouchableOpacity
-                            style={styles.loginButton}
-                            onPress={Validacion}
+                                style={styles.loginButton}
+                                onPress={Validacion}
                             >
                             <Text style={styles.loginButtonText}>
                                 Iniciar Sesión
